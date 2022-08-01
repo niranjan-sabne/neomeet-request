02c00838245169e8d2016e7ffde6e7b55331e639
VinayakDeulkar <vinayak.deulkar@neosoftmail.com>
2022-08-01 14:24:09 +0530

    dev : login module

---
+++ src/Components/LoginPage/Loginpage.tsx
@@ -0,0 +1,21 @@
+import { Col, Row } from 'antd'
+import Card from 'antd/lib/card/Card'
+import React from 'react'
+import Reusableimage from '../Reuseable/Reusableimage'
+
+const Loginpage: React.FC = () => {
+    return (
+        <div>
+            <Row>
+                <Col span={10}>
+                    
+                </Col>
+                <Col span={14}>
+                    <Reusableimage />
+                </Col>
+            </Row>
+        </div>
+    )
+}
+
+export default Loginpage
---
+++ src/Components/Reuseable/Reusableimage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "antd";
+import "./Reuseableimage.css";
+const Reusableimage: React.FC = () => {
+  return <>
+    <div className="reusableMainDiv">
+        <Typography.Title style={{width:'100%'}}>WelCome To Neo-Meet</Typography.Title>
+    </div>
+  </>;
+};
+
+export default Reusableimage;
